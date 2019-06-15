043bb62977e5ce15d6960245f949af73eeac7266
Taweesak Janwityanujit <twesakj@gmail.com>
2019-06-15 08:05:19 +0700

    Update const.js

--- module/js/model/const.js
+++ module/js/model/const.js
@@ -329,7 +329,6 @@ RADIOSURG = [
   ["br", "", "", ""],
   ["radio", "100", "doneby", "Staff Neuro"],
   ["radio", "100", "doneby", "Staff Radio"],
-  ["span", "120", "", ""]
 ],
 ENDOVASC = [
   ["hr", "inner", "", "", ""],
