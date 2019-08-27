2df39e487ba71b5222573c867c1bda4442933ec7
Taweesak Janwityanujit <twesakj@gmail.com>
2019-08-27 20:57:35 +0700

    neuro1

--- neuro1/module/line/lineNotify.php
+++ neuro1/module/line/lineNotify.php
@@ -8,8 +8,10 @@
 	$subdir = "imagefile/";
    
 	$line_api = 'https://notify-api.line.me/api/notify';
-//	$line_token = 'jyaKhr5MuY9jBeWbEzk2OjhT9ucAzCY9Q8ei3ieEGac'; // my LINE
-	$line_token = '2ItNh2j4Z1fIFCSWkZXBH4qtDYigXpl19ahsdWIR5pX'; // group LINE นิวโรศัลย์ รามา ปัจจุบัน
+	$line_token = 'jyaKhr5MuY9jBeWbEzk2OjhT9ucAzCY9Q8ei3ieEGac'; // my LINE
+	$line_token1 = '2ItNh2j4Z1fIFCSWkZXBH4qtDYigXpl19ahsdWIR5pX'; // group LINE นิวโรศัลย์ รามา ปัจจุบัน
+	$line_token2 = 'KKVhc6ZVdAHqGQtL23U9kvfT502cjprMNNbRTH9NEll'; // group LINE Navigator
+	$line_token3 = 'GDcs3oGABywMlCJAxlwT3egmcEDlnfMoA8mQTADKLXh'; // group LINE Nerve monitoring
 
 	// Use userID and time in seconds for filename
 	$t = microtime();
@@ -29,6 +31,13 @@
 		"imageFile" => new CURLFile($subdir . $filename)
 	);
 
+  curl($line_api, $data, $line_token);
+//  curl($line_api, $data, $line_token1);
+//  curl($line_api, $data, $line_token2);
+//  curl($line_api, $data, $line_token3);
+
+function curl($line_api, $data, $line_token)
+{
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $line_api);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
@@ -48,4 +57,5 @@
 	$server_output = curl_exec ($ch);
 
 	curl_close ($ch);
+}
 ?>
