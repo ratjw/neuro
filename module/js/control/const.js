85f6e77557dd219ad1bf72482c64b4f19316a5d3
Taweesak <twesakj@gmail.com>
2019-09-09 14:21:51 +0700

    no op

--- module/js/control/const.js
+++ module/js/control/const.js
@@ -763,8 +763,8 @@ ETCRXNO = [
 ],
 
 NOOPERATION = [
-  /adjust.*pressure/i, /advice.*surg[ery|ical]/i,
-  /conservative/i, /observe/i, /\boff OR/, /standby/i
+  /adjust.*pressure/i, /advice.*surg[ery|ical]/i, /conservative/i, 
+  /no op/i, /observe/i, /\boff OR/, /standby/i
 ],
 RADIOSURGERY = [
   /conformal radiotherapy/i, /CRT/i, /Cyber ?Knife/i,
