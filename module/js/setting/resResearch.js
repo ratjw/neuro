4137b60e50bf4515af442a3f50f35214e7bbc740
Taweesak Janwityanujit <twesakj@gmail.com>
2019-10-27 18:49:23 +0700

    Update resResearch.js

--- module/js/setting/resResearch.js
+++ module/js/setting/resResearch.js
@@ -153,7 +153,7 @@ function calcMulti(caption, width)
   Array.from(caption).forEach((e, i) => {
     chunk = remain.substring(0, i-x)
     if (context.measureText(chunk).width >= width) {
-      textline.push(remain.substring(0, i-x))
+      textline.push(remain.substring(0, i-x).trim())
       remain = caption.slice(i)
       x = i
     }
