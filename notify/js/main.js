1de5a592cc392f5b17a4bea5cc65c8972bf19d9c
ratjw <taweesak.jan@mahidol.ac.th>
2019-12-31 17:50:39 +0700

    timer at 6pm

--- notify/js/main.js
+++ notify/js/main.js
@@ -1,10 +1,51 @@
 
-import { start } from "./start.js"
+import { sqlStart } from "./sqlupdate.js"
+import { fillmain } from "./fill.js"
+import { updateBOOK } from "./updateBOOK.js"
+import { Alert } from "./util.js"
+import { notifyLINE } from './notifyLINE.js'
 
-export let USER = '000000'//sessionStorage.getItem("userid")
+// msec in a day
+const OneDay = 24 * 60 * 60 * 1000
 
-document.getElementById("wrapper").style.display = "block"
-document.getElementById("mainwrapper").style.height = window.innerHeight
-  - document.getElementById("cssmenu").style.height
+const timer = new Date;
+timer.setHours(18, 0, 0); // 6pm
+let msecRemain = ((timer - now) / 1000)
+if (msecRemain < 0)
+  msecRemain = msecRemain + (24 * 60 * 60 * 1000)
 
-start()
+setTimeout(run, secondsRemain)
+
+function run()
+{
+  setInterval(start, OneDay)
+}
+
+function start() {
+  sqlStart().then(response => {
+    typeof response === "object"
+    ? success(response)
+    : failed(response)
+  }).catch(error => alert(error.stack))
+}
+
+// Success return from server
+function success(response) {
+
+  document.getElementById("wrapper").style.display = "block"
+  document.getElementById("mainwrapper").style.height = window.innerHeight
+    - document.getElementById("cssmenu").style.height
+
+  updateBOOK(response)
+  fillmain()
+
+  notifyLINE()
+}
+
+// *** to do -> offline browsing by service worker ***
+function failed(response) {
+  let title = "Server Error",
+    error = error + "<br><br>Response from server has no data"
+
+  Alert(title, error + "No localStorage backup")
+}
