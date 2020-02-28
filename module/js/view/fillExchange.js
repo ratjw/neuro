295e6f3ea0b0172655fca1b3aebf6ee06988084e
Taweesak Janwityanujit <twesakj@gmail.com>
2020-02-28 08:19:13 +0700

    Update fillExchange.js

--- module/js/view/fillExchange.js
+++ module/js/view/fillExchange.js
@@ -50,8 +50,10 @@ function getActiveExchange(staffs)
 
 function fillExchngCell(cell, staffname, edittime)
 {
+  let celltime = cell.dataset.edittime ? Number(cell.dataset.edittime) : 0
+
   if (staffname === cell.dataset.consult) { return }
-  if (edittime < cell.dataset.edittime) { return }
+  if (edittime < celltime) { return }
   cell.dataset.edittime = edittime
   if (!cell.dataset.origconsult) {
     cell.dataset.origconsult = cell.dataset.consult
@@ -62,105 +64,3 @@ function fillExchngCell(cell, staffname, edittime)
 
   cell.dataset.consult = staffname
 }
-
-//=====================================
-function removeDupExchng(staffs)
-{
-  // convert from array of objects to an object of objects
-//  const staffsObj = Object.assign({}, ...staffs)
-  const eachExchDates = getEachExchDate(staffs)
-
-  return removeDups(eachExchDates)
-}
-
-function getEachExchDate(staffs)
-{
-  const eachExchDate = []
-
-  staffs.forEach(staff => {
-    Object.entries(staff).forEach(([name, exch]) => {
-      Object.entries(exch).forEach(([date, edit]) => {
-        let latestEdit = getLatestKey(edit)
-        let subObj = {[name]: {[date]: latestEdit}}
-        eachExchDate.push(subObj)
-      })
-    })
-  })
-
-  return eachExchDate
-}
-
-function removeDups(staffs)
-{
-  let dateArray = []
-  let timestamp = 0
-  let num = 0
-
-  staffs.forEach((staff, i) => {
-    Object.entries(staff).forEach(([name, exchng]) => {
-      Object.entries(exchng).forEach(([date, edit]) => {
-        let editTime = Object.keys(edit)[0]
-        if (timestamp === 0) {
-          dateArray.push(date)
-          timestamp = editTime
-          num = i
-          return
-        }
-        if (dateArray.includes(date)) {
-          if (editTime > timestamp) {
-            timestamp = editTime
-            staffs.splice(num, 1)
-          } else {
-            staffs.splice(i, 1)
-          }
-        } else {
-          dateArray.push(date)
-        }
-      })
-    })
-  })
-
-  return staffs
-}
-
-/*
-function removeDupExchng(staffs)
-{
-  // convert from array of objects to an object of objects
-  const staffsObj = Object.assign({}, ...staffs)
-  const exchngDates = Object.values(staffsObj).map(exchng => Object.keys(exchng).join())
-  const findDups = exchngDates.filter((e, i) => exchngDates.indexOf(e) !== i)
-  const dupDates = [...new Set(findDups)]
-
-  dupDates.forEach(dupDate => {
-    let timestamp = 0
-    let staffname = ''
-    Object.entries(staffsObj).forEach(([name, exchng]) => {
-      Object.entries(exchng).forEach(([date, edit]) => {
-        if (date === dupDate) {
-          let editTime = getLatestKey(edit)
-          if (timestamp === 0) {
-            timestamp = editTime
-            staffname = name
-            return
-          }
-          if (editTime > timestamp) {
-            timestamp = editTime
-            delete staffsObj[staffname][date]
-          } else {
-            delete staffsObj[name][date]
-          }
-        }
-      })
-    })
-  })
-
-  Object.entries(staffsObj).forEach(([name, exchng]) => {
-    if (Object.entries(exchng).length === 0) {
-      delete staffsObj[name]
-    }
-  })
-
-  return staffsObj
-}
-*/
