1b49ec560fd1a6695a86ea24aa14268ebca712ac
ratjw <taweesak.jan@mahidol.ac.th>
2020-05-27 17:14:33 +0700

    holiday

--- module/js/view/findHoliday.js
+++ module/js/view/findHoliday.js
@@ -5,17 +5,31 @@ import { getHOLIDAY } from "../util/updateBOOK.js"
 export function findHoliday(date)
 {
   const maxyear = "9999",
-    dayofweek = (new Date(date)).getDay(),
-    Sun = dayofweek === 0,
-    Sat = dayofweek === 6,
-    holiday = getHOLIDAY(),
-    thisyear = new Date().getFullYear(),
-    newHoliday = holiday.filter(day => day.holidate > thisyear),
-    thisHoliday = newHoliday.forEach(day => {
-      if (day.holidate > maxyear) {
-        day.holidate = day.holidate.replace(maxyear, thisyear)
-      }
-    })
+    thisyear = (new Date().getFullYear()).toString(),
+    allHoliday = fixHolidayX3(),
+    yesHoliday = allHoliday.find(holi => holi.holidate === date)
 
-  return thisHoliday[date] ||  ''
+  return yesHoliday ? yesHoliday.dayname :  ''
+}
+
+function fixHolidayX3()
+{
+  const maxyear = "9999",
+    fullyear = new Date().getFullYear(),
+    thisyear = fullyear.toString(),
+    nextyear = (fullyear + 1).toString(),
+    extyear = (fullyear + 2).toString(),
+    holiday = getHOLIDAY(),
+    newHoliday = holiday.filter(day =>
+                   (day.holidate > thisyear) && (day.holidate < maxyear)
+                 ),
+    fixHoliday1 = holiday.filter(day => day.holidate > maxyear),
+    fixHoliday2 = JSON.parse(JSON.stringify(fixHoliday1)),
+    fixHoliday3 = JSON.parse(JSON.stringify(fixHoliday1))
+
+  fixHoliday1.forEach(d => d.holidate = d.holidate.replace(maxyear, thisyear))
+  fixHoliday2.forEach(d => d.holidate = d.holidate.replace(maxyear, nextyear))
+  fixHoliday3.forEach(d => d.holidate = d.holidate.replace(maxyear, extyear))
+
+  return [...newHoliday, ...fixHoliday1, ...fixHoliday2, ...fixHoliday3]  
 }
--- module/js/view/rowDecoration.js
+++ module/js/view/rowDecoration.js
@@ -7,18 +7,31 @@ import { findHoliday } from "../view/findHoliday.js"
 const NAMEOFDAYFULL = ["Sunday", "Monday", "Tuesday",
   "Wednesday", "Thursday", "Friday", "Saturday"
 ]
+const COMPENSATE = "ชดเชย"
+const ENDYEAR = "วันสิ้นปี"
+const NEWYEAR = "วันขึ้นปีใหม่"
+const SONGKRAN = "วันสงกรานต์"
 
 export function rowDecoration(row, date)
 {
   const cells = row.cells,
-    cellDiag = cells[DIAGNOSIS]
+    cellDiag = cells[DIAGNOSIS],
+    dayofweek = (new Date(date)).getDay(),
+    Mon = dayofweek === 1,
+    Tue = dayofweek === 2
 
   row.className = dayName(NAMEOFDAYFULL, date) || "nodate"
   cells[OPDATE].innerHTML = putThdate(date)
 
   if (date < LARGESTDATE) {
-    cellDiag.classList.add("holiday")
-    cellDiag.dataset.holiday = findHoliday(date)
+    if (holidayName) {
+      cellDiag.classList.add("holiday")
+      cellDiag.dataset.holiday = findHoliday(date)
+    }
+    else if (row.rowIndex > 2) {
+      if (Mon) { compensateMon(row) }
+      else if (Tue) { compensateTue(row) }
+    }
   }
 }
 
@@ -27,4 +40,41 @@ function dayName(DAYNAME, date)
   return date === LARGESTDATE
     ? ""
     : DAYNAME[(new Date(date)).getDay()]
+}
+
+function compensateMon(row)
+{
+  const Sunday = row.previousElementSibling.previousElementSibling
+    Satday = Sunday.previousElementSibling,
+    cellSun = Sunday.cells[DIAGNOSIS],
+    cellSat = Satday.cells[DIAGNOSIS],
+    cellDiag = cells[DIAGNOSIS],
+    holidayName = ""
+
+  if (cellSat.className.includes("holiday")) {
+    holidayName = cellSat.dataset.holiday
+  }
+    cellDiag.classList.add("holiday")
+    cellDiag.dataset.holiday = COMPENSATE + holidayName
+}
+
+function compensateTue(row)
+{
+  const Sunday = row.previousElementSibling.previousElementSibling.previousElementSibling
+    Satday = Sunday.previousElementSibling,
+    cellSun = Sunday.cells[DIAGNOSIS],
+    cellSat = Satday.cells[DIAGNOSIS],
+    cellDiag = cells[DIAGNOSIS]
+
+  if ((holidayName === ENDYEAR) || (holidayName === NEWYEAR)) {
+    const Tuesday = Monday.previousElementSibling
+    if (!Tuesday) { return }
+    const cellTue = Tuesday.cells[DIAGNOSIS]
+    cellTue.classList.add("holiday")
+    cellTue.dataset.holiday = COMPENSATE + holidayName
+  }
+  else if (holidayName !== SONGKRAN) {
+    cellMon.classList.add("holiday")
+    cellMon.dataset.holiday = COMPENSATE + holidayName
+  }
 }
