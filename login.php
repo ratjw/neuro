1102e0a5bfaf44daee0f5be9422524cebe51b073
ratjw <taweesak.jan@mahidol.ac.th>
2020-05-30 16:24:46 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
 <?php
+include "connect.php";
+
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$userid = !empty($_POST['userid']) ? $_POST['userid'] : '';
 		$pwd = !empty($_POST['pwd']) ? $_POST['pwd'] : '';
@@ -14,24 +16,25 @@
 		$error = "";
 
 		if (preg_match('/^\d{6}$/', $userid)) {
-			if (strpos($thisname, $servername) !== false) {
+      $localhost = strpos($thisname, "localhost") !== false;
+      $localnet = strpos($thisname, "192.168") !== false;
+      $intranet = strpos($thisname, $servername) !== false;
+
+			if ($localhost || $localnet) {
+				$resultz = "S";
+			}
+			else if ($intranet) {
 				$client = new SoapClient($wsdl);
 				$resultx = $client->Get_staff_detail($userid, $pwd);
 				$resulty = simplexml_load_string($resultx);
 				$resultz = (string)$resulty->children()->children()->role;
 			}
-			else if (strpos($thisname, "localhost") !== false) {
-				$resultz = "S";
-			}
-			else if (strpos($thisname, "192.168") !== false) {
-				$resultz = "S";
-			}
 
 			$staff = $resultz === "S";
       $resident = $resultz === "R";
-      $secretary = $resultz === "G";
       $nurse = $resultz === "N";
-
+      $secretary = $resultz === "G";
+ 
       if ($staff || $resident || $secretary) {
 				header($browserDoctor);
 			}
