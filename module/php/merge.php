69fc65e5da5c6f78b9713b0c27bc5cdabbc218e8
ratjw <taweesak.jan@mahidol.ac.th>
2020-09-11 20:01:45 +0700

    Update merge.php

--- module/php/merge.php
+++ module/php/merge.php
@@ -1,7 +1,7 @@
 <?php
 function merge($input, $result)
 {
-  foreach ($input as $key) {
+  foreach ($input as $key => $val) {
     if (array_key_exists($key, $result)) {
       $input[$key] = $result[$key];
     }
