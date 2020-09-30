a4b71e97bf8c97538e96e9a6bc6708070913a8fe
ratjw <taweesak.jan@mahidol.ac.th>
2020-09-30 11:01:15 +0700

    Update sqlservice.js

--- module/js/model/sqlservice.js
+++ module/js/model/sqlservice.js
@@ -14,42 +14,7 @@ export function sqlGetIPD() {
 }
 
 export function sqlSaveService(column, content, qn) {
-  let sql = `sqlReturnService=${sqlItem(column, content, qn)}${sqlOneMonth()}`
+  const sql = `UPDATE book SET ${column}='${content}',editor='${USER}' WHERE qn=${qn};`
 
-  return postData(MYSQLIPHP, sql);
-}
-
-export function sqlGetUpdateService()
-{
-  let sql = "sqlReturnService=" + sqlOneMonth()
-
-  return postData(MYSQLIPHP, sql);
-}
-
-function sqlOneMonth()
-{
-  return `SELECT * FROM book b left join personnel p
-            ON JSON_CONTAINS(p.profile,JSON_QUOTE(b.staffname),"$.name")
-          WHERE opdate BETWEEN '${serviceFromDate}' AND '${serviceToDate}'
-            AND deleted=0
-            AND waitnum<>0
-            AND hn
-          ORDER BY p.id,opdate,oproom,casenum,waitnum;`
-}
-
-function sqlColumn(column, content, qn)
-{
-  return "sqlReturnService=" + sqlItem(column, content, qn)
-}
-
-function sqlDefaults(qn)
-{
-  return `UPDATE book
-          SET doneby='',scale='',manner='',editor='${USER}'
-          WHERE qn=${qn};`
-}
-
-function sqlItem(column, content, qn)
-{
-  return `UPDATE book SET ${column}='${content}',editor='${USER}' WHERE qn=${qn};`
+  return postData(MYSQLIPHP, { sqlReturnService: {serviceFromDate, serviceToDate, sql} })
 }
