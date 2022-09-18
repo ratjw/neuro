35bb2f3d325fa2249df74a6bd611bbed540e8de7
ratjw <taweesak.jan@mahidol.ac.th>
2022-09-19 06:03:23 +0700

    Update pagination.js

--- module/js/view/pagination.js
+++ module/js/view/pagination.js
@@ -5,12 +5,13 @@ import { viewEquip } from "./viewEquip.js"
 import { obj_2_ISO, ISO_2_th, nextdates } from "../util/date.js"
 import { winWidth, winHeight, winResizeFix } from "../util/util.js"
 
-export function pagination($dialog, $maintbl, book, search)
+export function pagination($dialog, $showtbl, found, search)
 {
-  let  beginday = book[0].opdate,
-    lastday = findLastDateInBOOK(book),
-    firstday = getPrevMonday(),
-    offset = 0
+  let week = 7,
+    month = 28,
+    year = 364,
+    previous = -1,
+    firstday
 
   $dialog.dialog({
     title: search,
@@ -18,8 +19,8 @@ export function pagination($dialog, $maintbl, book, search)
     modal: true,
     show: 200,
     hide: 200,
-    width: winWidth(95),
-    height: winHeight(95),
+    width: winWidth(100),
+    height: winHeight(100),
     close: function() {
       $(window).off("resize", resizeDialog )
       $(".fixed").remove()
@@ -29,21 +30,20 @@ export function pagination($dialog, $maintbl, book, search)
         text: "<<< Year",
         class: "yearbut",
         click: function () {
-          showOneWeek(book, firstday, -364)
+          showOneWeek(found, previous * year)
         }
       },
       {
         text: "<< Month",
         class: "monthbut",
         click: function () {
-          offset = firstday.slice(-2) > 28 ? -35 : -28
-          showOneWeek(book, firstday, offset)
+          showOneWeek(found, previous * month)
         }
       },
       {
         text: "< Week",
         click: function () {
-          showOneWeek(book, firstday, -7)
+          showOneWeek(found, previous * week)
         }
       },
       {
@@ -52,53 +52,65 @@ export function pagination($dialog, $maintbl, book, search)
       {
         text: "Week >",
         click: function () {
-          showOneWeek(book, firstday, 7)
+          showOneWeek(found, week)
         }
       },
       {
         text: "Month >>",
         class: "monthbut",
         click: function () {
-          offset = firstday.slice(-2) > 28 ? 35 : 28
-          showOneWeek(book, firstday, offset)
+          showOneWeek(found, month)
         }
       },
       {
         text: "Year >>>",
         class: "yearbut",
         click: function () {
-          showOneWeek(book, firstday, 364)
+          showOneWeek(found, year)
         }
       }
     ]
   })
 
-  showOneWeek(book, firstday, 0)
+  showOneWeek(found, 0)
 
   //for resizing dialogs in landscape / portrait view
   $(window).on("resize", resizeDialog )
 
-  function showOneWeek(book, firstday, offset) {
+  function showOneWeek(found, offset) {
     $('.fixed').remove()
-    showTheWeek(book, firstday, offset)
-    $maintbl.fixMe($dialog)
+    showTheWeek(found, offset)
+    $showtbl.fixMe($dialog)
   }
 
-  function showTheWeek(book, Monday, offset)
+  function showTheWeek(found, offset)
   {
-    let  bookOneWeek, Sunday
+    let opdates = found.map(e => e.opdate),
+      lastday = opdates.reduce((a, b) => a > b ? a : b),
+      beginday = found[0].opdate,
+      firstdayYear = new Date(firstday).getFullYear(),
+      MAXDATEYear = new Date(MAXDATE).getFullYear(),
+      Monday
 
-    firstday = nextdates(Monday, offset)
-    if (firstday < beginday) { firstday = getPrevMonday(beginday) }
-    if (firstday > lastday) {
-      firstday = nextdates(getPrevMonday(lastday), 7)
-      bookOneWeek = getBookNoDate(book)
-      showAllCases(bookOneWeek)
+    if ((firstdayYear === MAXDATEYear) && (offset < 0)) {
+      opdates = opdates.filter(e => e !== MAXDATE)
+      lastday = opdates.reduce((a, b) => a > b ? a : b)
+      Monday = getPrevMonday(lastday)
     } else {
-      Sunday = getNextSunday(firstday)
-      bookOneWeek = getBookOneWeek(book, firstday, Sunday)
-      showAllCases(bookOneWeek, firstday, Sunday)
+      Monday = offset ? nextdates(firstday, offset) : getPrevMonday()
     }
+
+    if ((Monday < beginday) || (Monday > lastday)) return
+
+  //    Monday = nextdates(getPrevMonday(lastday), 7)
+  //    bookOneWeek = getBookNoDate(found)
+  //    showAllCases(bookOneWeek)
+    
+    const bookOneWeek = getBookOneWeek(found, Monday, offset),
+      date = bookOneWeek[0].opdate,
+      newMonday = getPrevMonday(date)
+
+    showAllCases(bookOneWeek, newMonday)
   }
 
   function getPrevMonday(date)
@@ -117,64 +129,89 @@ export function pagination($dialog, $maintbl, book, search)
     return obj_2_ISO(today);
   }
 
-  function getBookOneWeek(book, Monday, Sunday)
+  function getBookOneWeek(found, Monday, offset)
   {
-    return $.grep(book, function(q) {
-      return q.opdate >= Monday && q.opdate <= Sunday
-    })
+    const Sunday = getNextSunday(Monday),
+      data = getBookThisWeek(found, Monday, Sunday)
+
+    return data.length ? data : getNotEmptyWeek(found, Monday, Sunday, offset)
   }
 
-  function getBookNoDate(book)
+  function getBookThisWeek(found, Monday, Sunday)
   {
-    return $.grep(book, function(q) {
-      return q.opdate === MAXDATE
-    })
+    return found.filter(e => (e.opdate >= Monday) && (e.opdate <= Sunday))
   }
 
-  function showAllCases(bookOneWeek, Monday, Sunday)
+  function getNotEmptyWeek(found, Monday, Sunday, offset)
   {
-    let  Mon = Monday && ISO_2_th(Monday) || "",
+    const opdate = findNotEmptyWeek(found, Monday, Sunday, offset)
+
+    if (opdate === MAXDATE) {
+      return getBookNoDate(found)
+    }
+
+    const newMonday = getPrevMonday(opdate),
+    newSunday = getNextSunday(newMonday)
+
+    return getBookThisWeek(found, newMonday, newSunday)
+  }
+
+  // slice() before reverse() to prevent array mutation
+  function findNotEmptyWeek(found, Monday, Sunday, offset)
+  {
+    return offset < 0
+            ? found.slice().reverse().find(e => e.opdate < Monday).opdate
+            : found.find(e => e.opdate > Sunday).opdate
+  }
+
+  function getBookNoDate(found)
+  {
+    return found.filter(e => e.opdate === MAXDATE)
+  }
+
+  function showAllCases(bookOneWeek, Monday)
+  {
+    let Sunday = getNextSunday(Monday),
+      Mon = Monday && ISO_2_th(Monday) || "",
       Sun = Sunday && ISO_2_th(Sunday) || ""
+
+    firstday = Monday
 
     $dialog.dialog({
       title: search + " : " + Mon + " - " + Sun
     })
     // delete previous table lest it accumulates
-    $maintbl.find('tr').slice(1).remove()
+    $showtbl.find('tr').slice(1).remove()
 
     if (Monday) {
-      let  $row, row, cells,
+      let  $row, row,
         date = Monday,
         nocase = true
 
       $.each( bookOneWeek, function() {
         while (this.opdate > date) {
           if (nocase) {
-            $row = $('#allcells tr').clone().appendTo($maintbl.find('tbody'))
-            row = $row[0]
-            cells = row.cells
-            rowDecoration(row, date)
+            $row = $('#allcells tr').clone().appendTo($showtbl.find('tbody'))
+            rowDecoration($row[0], date)
           }
           date = nextdates(date, 1)
           nocase = true
         }
         $('#allcells tr').clone()
-          .appendTo($maintbl.find('tbody'))
+          .appendTo($showtbl.find('tbody'))
             .filldataAllcases(this)
         nocase = false
       })
       date = nextdates(date, 1)
       while (date <= Sunday) {
-        $row = $('#allcells tr').clone().appendTo($maintbl.find('tbody'))
-        row = $row[0]
-        cells = row.cells
-        rowDecoration(row, date)
+        $row = $('#allcells tr').clone().appendTo($showtbl.find('tbody'))
+        rowDecoration($row[0], date)
         date = nextdates(date, 1)
       }
-    } else {
+    } else {// MAXDATE
       $.each( bookOneWeek, function() {
         $('#allcells tr').clone()
-          .appendTo($maintbl.find('tbody'))
+          .appendTo($showtbl.find('tbody'))
             .filldataAllcases(this)
       });
     }
@@ -182,10 +219,10 @@ export function pagination($dialog, $maintbl, book, search)
 
   function resizeDialog() {
     $dialog.dialog({
-      width: winWidth(95),
-      height: winHeight(95)
+      width: winWidth(100),
+      height: winHeight(100)
     })
-    winResizeFix($maintbl, $dialog)
+    winResizeFix($showtbl, $dialog)
   }
 }
 
@@ -210,10 +247,3 @@ jQuery.fn.extend({
     rowDecoration(row, date)
   }
 })
-
-function findLastDateInBOOK(book)
-{
-  let opdates = book.map(e => e.opdate)
-
-  return opdates.reduce((a, b) => a > b ? a : b)
-}
