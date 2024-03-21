cb6e5a0fa44eae2d5b4eeaaf03c1bec29010589f
ratjw <taweesak.jan@mahidol.ac.th>
2024-03-21 15:29:01 +0700

    add menu

--- module/js/model/sqlCaseAll.js
+++ module/js/model/sqlCaseAll.js
@@ -2,7 +2,7 @@
 import { postData, MYSQLIPHP } from "./fetch.js"
 
 export function sqlCaseAll() {
-  let sql = `SELECT * FROM book WHERE deleted=0 ORDER BY opdate;`
+  let sql = `SELECT * FROM book WHERE deleted=0 AND hn>"0000000" ORDER BY opdate;`
 
   return postData(MYSQLIPHP, {sqlReturnData:sql})
 }
