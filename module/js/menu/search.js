e39770887c7eee5478dc289bc235b75d39fef2ff
ratjw <taweesak.jan@mahidol.ac.th>
2024-03-31 19:34:40 +0700

    re-arrange search routine

--- module/js/menu/search.js
+++ module/js/menu/search.js
@@ -6,13 +6,8 @@ Search in specific column :
 Forgotten word part       *
 */
 
-import { sqlSearchDB } from "../model/sqlSearchDB.js"
-import { Alert, reposition, menustyle } from "../util/util.js"
+import { reposition, menustyle } from "../util/util.js"
 import { clearAllEditing } from "../control/clearAllEditing.js"
-import { viewSearchDB } from "../view/viewSearchDB.js"
-import { prepareHTMLtoExcel } from "../util/prepareHTMLtoExcel.js"
-import { downloadJSONasCSV } from "../util/downloadJSONasCSV.js"
-import { inputPeriod } from "../util/inputPeriod.js"
 
 export function search(destine)
 {
@@ -68,44 +63,4 @@ function getSaffName(pointing, $staffsearch)
 
   reposition($staffsearch, "left top", "left bottom", $pointing)
   menustyle($staffsearch, $pointing)
-}
-
-function searchDB(destine)
-{
-  let hn = $('#dialogInput input[name="hn"]').val().trim(),
-    name = $('#dialogInput input[name="name"]').val().trim(),
-    surname = $('#dialogInput input[name="surname"]').val().trim(),
-    staffname = $('#dialogInput input[name="staffname"]').val().trim(),
-    others = $('#dialogInput input[name="others"]').val().trim(),
-    datebegin = $('#dialogInput input[id="dateFrom"]').val().trim(),
-    dateend = $('#dialogInput input[id="dateTo"]').val().trim(),
-    fullname = name + (surname ? " " : "") + surname,
-    search = ""
-
-  // Close before open another dialog
-  $("#dialogInput").dialog("close")
-
-  // for dialog title
-  search += hn
-  search += (search && fullname ? ", " : "") + fullname
-  search += (search && staffname ? ", " : "") + staffname
-  search += (search && others ? ", " : "") + others
-  search += (search && datebegin ? ", " : "") + datebegin
-  search += (search && dateend ? ", " : "") + dateend
-
-  if (!search) { return }
-
-  sqlSearchDB(hn, name, surname, staffname, others, datebegin, dateend).then(response => {
-    if (typeof response === "object") {
-      destine == "screen"
-      ? viewSearchDB(response, search)
-      : destine == "excel"
-        ? prepareHTMLtoExcel(response, search)
-        : destine == "csv"
-          ? downloadJSONasCSV(response, search)
-          : null
-    } else {
-      Alert("Search: " + search, response)
-    }
-	}).catch(error => alert(error.stack))
 }
---
+++ module/js/menu/searchDB.js
@@ -0,0 +1,47 @@
+
+import { sqlSearchDB } from "../model/sqlSearchDB.js"
+import { Alert } from "../util/util.js"
+import { viewSearchDB } from "../view/viewSearchDB.js"
+import { prepareHTMLtoExcel } from "../util/prepareHTMLtoExcel.js"
+import { downloadJSONasCSV } from "../util/downloadJSONasCSV.js"
+import { inputPeriod } from "../util/inputPeriod.js"
+
+function searchDB(destine)
+{
+  let hn = $('#dialogInput input[name="hn"]').val().trim(),
+    name = $('#dialogInput input[name="name"]').val().trim(),
+    surname = $('#dialogInput input[name="surname"]').val().trim(),
+    staffname = $('#dialogInput input[name="staffname"]').val().trim(),
+    others = $('#dialogInput input[name="others"]').val().trim(),
+    datebegin = $('#dialogInput input[id="dateFrom"]').val().trim(),
+    dateend = $('#dialogInput input[id="dateTo"]').val().trim(),
+    fullname = name + (surname ? " " : "") + surname,
+    search = ""
+
+  // Close before open another dialog
+  $("#dialogInput").dialog("close")
+
+  // for dialog title
+  search += hn
+  search += (search && fullname ? ", " : "") + fullname
+  search += (search && staffname ? ", " : "") + staffname
+  search += (search && others ? ", " : "") + others
+  search += (search && datebegin ? ", " : "") + datebegin
+  search += (search && dateend ? ", " : "") + dateend
+
+  if (!search) { return }
+
+  sqlSearchDB(hn, name, surname, staffname, others, datebegin, dateend).then(response => {
+    if (typeof response === "object") {
+      destine == "screen"
+      ? viewSearchDB(response, search)
+      : destine == "excel"
+        ? prepareHTMLtoExcel(response, search)
+        : destine == "csv"
+          ? downloadJSONasCSV(response, search)
+          : null
+    } else {
+      Alert("Search: " + search, response)
+    }
+	}).catch(error => alert(error.stack))
+}
