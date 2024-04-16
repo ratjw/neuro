db8541dfff74b26fd8f31a9bc74442e8e1f74669
ratjw <taweesak.jan@mahidol.ac.th>
2024-04-17 05:51:58 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,53 +2,83 @@
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$userid = !empty($_POST['userid']) ? $_POST['userid'] : '';
 		$pwd = !empty($_POST['pwd']) ? $_POST['pwd'] : '';
-		$browser = !empty($_POST['browser']) ? $_POST['browser'] : '';
+		$doctor = !empty($_POST['browser']) ? $_POST['browser'] : '';
+    $admin = ($userid === "000000") && ($pwd === "BzAs1953");
 
-		$browserDoctor = "location:$browser";
-		$browserNurse = "location:nurse/nurse.html";
-
-		$servername = "surgery.rama.mahidol.ac.th";
     $thisname = $_SERVER["SERVER_NAME"];
-		$wsdl="http://appcenter/webservice/patientservice.wsdl";
-		$resultz = "";
-		$error = "";
-    $personnel = preg_match('/^\d{6}$/', $userid);
+    $localhost = strpos($thisname, "localhost") !== false;
+    $localnet = strpos($thisname, "192.168") !== false;
 
-		if ($personnel) {
-      $localhost = strpos($thisname, "localhost") !== false;
-      $localnet = strpos($thisname, "192.168") !== false;
-      $intranet = strpos($thisname, $servername) !== false;
-
-			if ($localhost || $localnet) {
-				$resultz = "S";
-			}
-      else if ($intranet) {
-				$client = new SoapClient($wsdl);
-				$resultx = $client->Get_staff_detail($userid, $pwd);
-				$resulty = simplexml_load_string($resultx);
-				$resultz = (string)$resulty->children()->children()->role;
-			}
-
-      $admin = $userid === "000000" && $pwd === "BzAs1953";
-			$staff = $resultz === "S";
-			$fellow = $resultz === "F";
-      $resident = $resultz === "R";
-      $nurse = $resultz === "N";
-      $secretary = $resultz === "G";
- 
-      if ($admin || $staff || $fellow || $resident || $secretary) {
-				header($browserDoctor);
-			}
-			else if ($nurse) {
-				header($browserNurse);
-			}
-			else {
+    // exclude non-Rama registers
+    if (!$localhost && !$localnet && !$admin) {
+      $success = getLogin($userid, $pwd);
+      if (!$success) {
 				$error = "Incorrect password or username";
         header("location:index.php?error=$error");
-			}
-		} else {
-			$error = "Invalid username";
+        return;
+      }
+    }
+
+    $neurosurgery = getOrgId($userid);
+
+		// exclude non-neurosurgery personnels
+    if (!$neurosurgery) {
+			$error = "For Neurosurgery Personnels Only";
       header("location:index.php?error=$error");
 		}
+
+    echo "<script>localStorage.setItem('role', '$neurosurgery');</script>";
+		$browserDoctor = "location:$doctor";
+		$browserNurse = "location:nurse/nurse.html";
+    if ($neurosurgery === "N") {
+      header($browserNurse);
+    }	else {
+      header($browserDoctor);
+    }
 	}
+
+function getLogin($userid, $pwd)
+{
+  $wsdl="http://appcenter/webservice/patientservice.wsdl";
+  $client = new SoapClient($wsdl);
+  $response = $client->Get_staff_detail($userid, $pwd);
+  $xml = simplexml_load_string($response);
+  $roleCode = (string)$xml->staff_detail->role;
+
+  return !!$roleCode;
+}
+
+// check personnel and role
+// use key 'http' even if you send the request to https://...
+function getOrgId($userid)
+{
+  $neurosurg = "50010566";
+  $url = 'http://prod-nodeserv.rama.mahidol.ac.th:8080/AuthenticationAPI/services/staffservice/getstaffinfobyid';
+  $data = json_encode(['staffId' => $userid]);
+  $options = [
+      "http" => [
+          "header" => "Content-Type: application/json",
+          "method" => "POST",
+          "content" => $data
+      ]
+  ];
+
+  $context = stream_context_create($options);
+  $result = file_get_contents($url, false, $context);
+  $resultz = json_decode($result);
+  $orgID = $resultz->data->orgId;
+  $roleCode = $resultz->data->roleCode;
+  $positionName = $resultz->data->positionName;
+
+  if ($userid === "000000") {
+    return true;
+  }
+  if ($orgID === $neurosurg) {
+    if ($roleCode === "S" && $positionName === "แพทย์") {
+      return "F";
+    } else {
+      return $roleCode;
+    }
+  }
+}
 ?>
