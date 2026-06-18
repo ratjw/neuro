52e4aa50866b2179635adc630033f79fb38a4958
ratjw <taweesak.jan@mahidol.ac.th>
2026-06-18 16:56:32 +0700

    Update tooltip.js

--- module/js/control/startsub/tooltip.js
+++ module/js/control/startsub/tooltip.js
@@ -7,20 +7,14 @@ import {
 export function tooltip()
 {
   let mainth = document.querySelectorAll("#maintbl tr:has(th)")
-  let servicetreatment = document.querySelectorAll("#maintbl th")
-  let retrace = document.querySelectorAll("#maintbl th")
-  let add = document.querySelectorAll("#maintbl th")
-  let postpone = document.querySelectorAll("#maintbl th")
-  let move = document.querySelectorAll("#maintbl th")
-  let copy = document.querySelectorAll("#maintbl th")
-  let del = document.querySelectorAll("#maintbl th")
-  let holiday = document.querySelectorAll("#maintbl th")
-  let staff = document.querySelectorAll("#maintbl th")
-  let interholiday = document.querySelectorAll("#maintbl th")
-  let general = document.querySelectorAll("#maintbl th")
+  let cssmenu = document.querySelectorAll("#cssmenu [id]")
 ;
   [...mainth].forEach(tr => {
     attachTooltip([...tr.querySelectorAll("th")])
+  })
+;
+  [...cssmenu].forEach(id => {
+    checkTooltip(id)
   })
 }
 
@@ -47,39 +41,41 @@ function attachTooltip(tooltipElements)
  
 function getMessage(e)
 {
-  let target = /*e.target.closest("table").id + */e.target.cellIndex
+  let target = e.target.cellIndex
 
-  return setMessage(target)
+  return setMainMessage(target)
 }
 
-function setMessage(column)
+function setMainMessage(column)
 {
   switch(column)
   {
     case OPDATE: return messageOPDATE(); break
-    case OPROOM: return messageOPROOM();; break
-    case OPTIME: return messageOPTIME();; break
-    case CASENUM: return messageCASENUM();; break
-    case STAFFNAME: return messageSTAFFNAME();; break
-    case HN: return messageHN();; break
-    case PATIENT: return messagePATIENT();; break
-    case DIAGNOSIS: return messageDIAGNOSIS();; break
-    case TREATMENT: return messageTREATMENT();; break
-    case EQUIPMENT: return messageEQUIPMENT();; break
-    case CONTACT: return messageCONTACT();; break
+    case OPROOM: return messageOPROOM(); break
+    case OPTIME: return messageOPTIME(); break
+    case CASENUM: return messageCASENUM(); break
+    case STAFFNAME: return messageSTAFFNAME(); break
+    case HN: return messageHN(); break
+    case PATIENT: return messagePATIENT(); break
+    case DIAGNOSIS: return messageDIAGNOSIS(); break
+    case TREATMENT: return messageTREATMENT(); break
+    case EQUIPMENT: return messageEQUIPMENT(); break
+    case CONTACT: return messageCONTACT(); break
   }
 }
 
 function messageOPDATE()
 {
   return (
-`คอลัมน์ Date - คลิกเลือกเคส
+`คอลัมน์ Date - คลิกเลือกเคส เลือกได้ทีละเคสเท่านั้น
  
  การเปลี่ยนวันผ่าตัด ทำได้ 2 วิธี
 	1. Drag & Drop ลากข้ามตารางได้
 	2. คลิกเลือกเคส ตรงช่องซ้ายสุดของเคสที่ต้องการ ->
           คลิก Move ตรงเมนูแถวบน ->
-          คลิกตรงวันที่ต้องการ`)
+          คลิกตรงวันที่ต้องการ
+
+เมื่อคลิกเลือกเคส จะปรากฏเมนูแถวบน เป็นตัวเลือกขั้นต่อไป`)
 }
 
 function messageOPROOM()
@@ -118,46 +114,40 @@ function messageCASENUM()
 function messageSTAFFNAME()
 {
   return (
-`คอลัมน์ Staff - คลิกเลือกชื่ออาจารย์เจ้าของไข้`)
+"คอลัมน์ Staff - คลิกเลือกชื่ออาจารย์เจ้าของไข้")
 }
 
 function messageHN()
 {
   return (
-`คอลัมน์ HN - ใส่ได้เฉพาะเมื่อยังว่าง`)
+"คอลัมน์ HN - ใส่ได้เฉพาะเมื่อยังว่าง")
 }
 
 function messagePATIENT()
 {
    return (
-`คอลัมน์ Patient - วันเสาร์เป็นชื่ออาจารย์ Consult`)
+"คอลัมน์ Patient - วันเสาร์เป็นชื่ออาจารย์ Consult")
 }
 
 function messageDIAGNOSIS()
 {
   return (
-`Login ID      เลขประจำตัว 6 หลัก            
-Password     ที่ใช้กับโรงพยาบาล`)
+`ชื่อโรคทั้งหมด
+
+ใส่ข้อความประกาศ`)
 }
 
 function messageTREATMENT()
 {
   return (
-`การบันทึกข้อมูล
-   เมื่อพิมพ์ข้อความเสร็จ ให้บันทึกโดย
-   1. คลิกที่ช่องอื่น
-   2. คลิกที่แถบคั่นสัปดาห์
-   3. กด Tab (เลื่อนไปช่องต่อไป) 
-   4. กด Shift+Tab (เลื่อนไปช่องย้อนหลัง)
-   5. โปรแกรมจะบันทึก ’ข้อมูลใหม่’ เอง เมื่ออยู่นิ่ง 10 วินาที`)
+`ชื่อการผ่าตัด
+
+ใส่ข้อความประกาศ`)
 }
 
 function messageEQUIPMENT()
 {
-   return (
-`Esc ยกเลิก ไม่บันทึก (ก่อน 10 วินาที)
-
-Enter ไม่ได้บันทึก แต่ไปขึ้นบรรทัดใหม่ในช่องเดิม`)
+   return ("แบบฟอร์มที่เคยใส่ข้อมูลแล้ว ต้องคลิก ‘แก้ไข’ ก่อน")
 }
 
 function messageCONTACT()
@@ -167,20 +157,28 @@ function messageCONTACT()
      Notes - ฐานะการเงิน ยาป้องกันเลือดแข็งตัว แพ้ยา`)
 }
 
-/*
-
-Menu แถวบน
-	Staff เป็น dropdown รายชื่ออาจารย์
+function messageStaff()
+{
+   return (
+`เป็น dropdown รายชื่ออาจารย์
          คลิกเลือกชื่ออาจารย์
 	       จะมีการแบ่งครึ่งหน้าจอ
              ด้านซ้าย เป็นตารางรวม
 	       ด้านขวา เป็นตารางของอาจารย์ท่านนั้น
-	       ใช้เมาส์ชี้ที่เส้นแบ่งกลาง เปลี่ยนความกว้าง ซ้าย|ขวา 
-	Service Review
-	      ต้องการเปลี่ยนเดือน คลิกหัวลูกศร ซ้าย ขวา
-	      ต้องลงเคสที่ตารางรวมก่อน ไม่สามารถลงเคสที่ตารางนี้
-	Search
-		หาเคส  ด้วย HN, ชื่อ, นามสกุล
+	       ใช้เมาส์ชี้ที่เส้นแบ่งกลาง เปลี่ยนความกว้าง ซ้าย|ขวา`)
+}
+
+function messageServiceReview()
+{
+   return (
+`ต้องการเปลี่ยนเดือน คลิกหัวลูกศร ซ้าย ขวา
+	      ต้องลงเคสที่ตารางรวมก่อน ไม่สามารถลงเคสที่ตารางนี้`)
+}
+
+function messageSearch()
+{
+   return (
+`หาเคส  ด้วย HN, ชื่อ, นามสกุล
 			            Staff
 			            คำใดๆ ที่ต้องการ
 All Saved Cases
@@ -189,26 +187,74 @@ All Saved Cases
      คลิกด้านล่างเลื่อนดู ย้อนหน้า-ย้อนหลัง
 All Deleted Cases
      เคสที่ถูกลบออกไป (แสดงเพียง 3 เดือนสุดท้าย)
-		     คลิกที่ช่องซ้ายสุดของเคส เพื่อ Undelete
-   
-   เมื่อคลิกช่องซ้ายสุด เป็นการเลือกเคสเดียว
-      เมนูแถวบน จะเพิ่มตัวเลือก
-      Retrace    ดูประวัติการแก้ไข
-	Add           เพิ่มเคสในวันเดียวกัน
-	Postpone  เลื่อนวันไปไม่มีกำหนด ต้องมีชื่อ Staff ก่อน
-	Move         คลิก move แล้วเลื่อนเมาส์ไป
- คลิกตรงวันที่ต้องการ
-      Copy         คลิก copy แล้วเลื่อนเมาส์ไป
- คลิกตรงวันที่ต้องการ
+		     คลิกที่ช่องซ้ายสุดของเคส เพื่อ Undelete`)
+}
 
-Equipment
-	แบบฟอร์มที่เคยใส่ข้อมูลแล้ว ต้องคลิก ‘แก้ไข’ ก่อน
+function messageRetrace()
+{
+   return ("ดูประวัติการแก้ไข")
+          
+}
 
+function messageAdd()
+{
+   return ("เพิ่มเคสในวันเดียวกัน")
+}
 
-Setting
-	Staff Setting		อัพเดทรายชื่ออาจารย์
-	วันหยุดปฏิทินสากล	วันหยุดนักขัตฤกษ์ วันที่ ตรงกันทุกปี
-			สำหรับ	วันหยุดนักขัตฤกษ์ วันที่ ไม่ตรงกันทุกปี
-ผู้ใช้ต้องกำหนดเอง
-	Readme
-*/
+function messagePostpone()
+{
+   return ("เลื่อนวันไปไม่มีกำหนด ต้องมีชื่อ Staff ก่อน")
+}
+
+function messageMove()
+{
+   return ("คลิก move แล้วเลื่อนเมาส์ไป")
+}
+
+function messageCopy()
+{
+   return ("คลิก copy แล้วเลื่อนเมาส์ไปคลิกตรงวันที่ต้องการ")
+}
+
+function messageDel()
+{
+   return ("ลบเคสที่เลือกไว้ ต้องยืนยันก่อนเสมอ")
+}
+
+function messageHoliday()
+{
+   return ("วันหยุดทางศาสนา วันที่ ไม่ตรงกันทุกปี ผู้ใช้ต้องกำหนดเอง")
+}
+
+function messageStaffSetting()
+{
+   return ("อัพเดทรายชื่ออาจารย์")
+}
+
+function messagePublic()
+{
+   return ("วันหยุดปฏิทินสากล	วันที่ ตรงกันทุกปี")
+}
+
+function messageReadme()
+{
+   return ("วันหยุดทางศาสนา วันที่ ไม่ตรงกันทุกปี ผู้ใช้ต้องกำหนดเอง")
+}
+
+function messageGeneral()
+{
+  return (
+`Login ID      เลขประจำตัว 6 หลัก            
+Password     ที่ใช้กับโรงพยาบาล
+การบันทึกข้อมูล
+   เมื่อพิมพ์ข้อความเสร็จ ให้บันทึกโดย
+   1. คลิกที่ช่องอื่น
+   2. คลิกที่แถบคั่นสัปดาห์
+   3. กด Tab (เลื่อนไปช่องต่อไป) 
+   4. กด Shift+Tab (เลื่อนไปช่องย้อนหลัง)
+   5. โปรแกรมจะบันทึก ’ข้อมูลใหม่’ เอง เมื่ออยู่นิ่ง 10 วินาที
+
+Esc ยกเลิก ไม่บันทึก (ก่อน 10 วินาที)
+
+Enter ไม่ได้บันทึก แต่ไปขึ้นบรรทัดใหม่ในช่องเดิม`)
+}
